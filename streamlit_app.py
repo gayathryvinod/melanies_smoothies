3a3d49dc234950436e51a7e25d84d7ff7b6df8a9
Gayathry Vinod <143829778+gayathryvinod@users.noreply.github.com>
2026-04-21 10:29:05 +0530

    Update streamlit_app.py
    changed 'Snowflake' to 'snowflake' connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
   """Choose the fruits you want in your custom Smoothie!""")
 
-cnx = st.connection("Snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
