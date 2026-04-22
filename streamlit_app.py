6753b1ffc7aa8d477db7e80e29479afe96993eaf
Gayathry Vinod <143829778+gayathryvinod@users.noreply.github.com>
2026-04-23 00:14:26 +0530

    Update streamlit_app.py
    checking iloc value for search val

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,8 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-#st.dataframe(data = my_dataframe, use_container_width = True)
-#st.stop()
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+
 
 name_on_order = st.text_input('Name on Smoothie')
 st.write('Name on your Smoothie will be:', name_on_order)
@@ -25,10 +22,13 @@ ingredients_list = st.multiselect('Choose upto 5 ingredients:', my_dataframe, ma
 
 if ingredients_list:
     ingredients_string = ''
-
-
     for fruit_chosen in ingredients_list:
       ingredients_string += fruit_chosen + ' '
+
+      search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
+      
       st.subheader(fruit_chosen + ' Nutritional Information')
       response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
       # Ensure we got a valid response before trying to display it
